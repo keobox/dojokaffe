3d1936e31c8fcd2fa53df0db2690b5cb3024fad6
Cesare Placanica <ugengia-git@yahoo.it>
2013-06-20 13:47:24 -0700

    Indentation?

--- fp/spring-elapsed/src/main/java/dojokaffe/fp/aop/Main.java
+++ fp/spring-elapsed/src/main/java/dojokaffe/fp/aop/Main.java
@@ -5,12 +5,12 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class Main {
 
-	/**
-	 * @param args
-	 */
-	public static void main(String[] args) {
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
         ApplicationContext context = new ClassPathXmlApplicationContext("elapsed-context.xml");
         MonitoredTask task = (MonitoredTask) context.getBean(MonitoredTask.class);
         task.executeTask();
-	}
+    }
 }
